495eba4bbae16066f5ee1ee6d169ed4e5fcf4291
huangwenwei <mr.huangwenwei@gmail.com>
2018-05-22 15:44:57 +0800

    Add log

--- main.go
+++ main.go
@@ -10,4 +10,5 @@ func main() {
 
 func init() {
 	config = new(configure)
+	initLogger()
 }
--- util.go
+++ util.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"log"
+	"io"
+	"os"
+	"strings"
 
 	homedir "github.com/mitchellh/go-homedir"
+	log "github.com/sirupsen/logrus"
 )
 
 // HomeDir 获取服务器当亲用户目录路径
@@ -13,4 +16,21 @@ func HomeDir() string {
 		log.Fatal(err.Error())
 	}
 	return home
+}
+
+func initLogger() {
+	path := strings.Join([]string{HomeDir(), ".ethereum_service"}, "/")
+	if err := os.MkdirAll(path, 0700); err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	filepath := strings.Join([]string{path, "out.log"}, "/")
+	file, err := os.OpenFile(filepath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	mw := io.MultiWriter(os.Stdout, file)
+	if err == nil {
+		log.SetOutput(mw)
+		log.Info("Note: all operate is recorded to log")
+	} else {
+		log.Error(err.Error())
+	}
 }
