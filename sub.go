2f2dabebadf31f369972ba7c850f8f308bd95600
huangwenwei <mr.huangwenwei@gmail.com>
2018-06-12 15:41:52 +0800

    Refactor sub command

--- sub.go
+++ sub.go
@@ -31,32 +31,93 @@ func subNewBlockCmd() {
 
 	blockCh := make(chan *types.Header)
 	sub, err := nodeClient.SubscribeNewHead(ctx, blockCh)
+
 	if err != nil {
 		log.Error(err.Error())
 	}
 
+	var (
+		// maintain orderHeight and increase 1 each subscribe callback, because head.number would jump blocks
+		orderHeight = new(big.Int)
+	)
 	for {
 		select {
 		case err := <-sub.Err():
 			log.Fatalln(err.Error())
 		case head := <-blockCh:
-			block, err := nodeClient.BlockByNumber(ctx, head.Number)
+			ordertmp, err := subHandle(orderHeight, head, nodeClient, esClient)
 			if err != nil {
-				log.Fatalln(err.Error())
+				log.Errorln(err.Error())
 			}
-			txs := block.Transactions()
-			for _, tx := range txs {
-				to := tx.To().Hex()
-				from, balance, pendingNonceAt, err := addressWithAmount(nodeClient, esClient, to)
-				if err != nil {
-					log.Warnln(err.Error())
-					continue
-				}
+			orderHeight = ordertmp
+		}
+	}
+}
 
-				if err := applyWithdrawAndConstructRawTx(balance, pendingNonceAt, nodeClient, from, &(config.To)); err != nil {
-					log.Errorln(err.Error())
-				}
-			}
+func subHandle(orderHeight *big.Int, head *types.Header, nodeClient *ethclient.Client, esClient *elastic.Client) (*big.Int, error) {
+	ctx := context.Background()
+	number := head.Number
+	originBlock, err := nodeClient.BlockByNumber(ctx, number)
+	if err != nil {
+		return nil, errors.New(strings.Join([]string{"get origin block error, height:", number.String(), err.Error()}, " "))
+	}
+
+	number.Sub(number, big.NewInt(1))
+	parentBlock, err := nodeClient.BlockByNumber(ctx, number)
+	if err != nil {
+		return nil, errors.New(strings.Join([]string{"get parent block error", number.String(), err.Error()}, " "))
+	}
+
+	if originBlock.ParentHash().Hex() != parentBlock.Hash().Hex() {
+		return nil, errors.New(strings.Join([]string{"uncle block, stable block's  height:", originBlock.Number().String()}, " "))
+	}
+
+	if orderHeight.Cmp(big.NewInt(0)) == 0 {
+		orderHeight = originBlock.Number()
+	}
+
+	var pushJumpBlock string
+	log.Infoln("sub message coming,", "order height:", orderHeight.Int64(), "sub block height:", originBlock.Number().Int64())
+	for blockNumber := orderHeight.Int64(); blockNumber <= originBlock.Number().Int64(); blockNumber++ {
+		block, err := nodeClient.BlockByNumber(ctx, big.NewInt(blockNumber))
+		if err != nil {
+			log.Warnln("Get block error, height:", blockNumber)
+			continue
+		}
+
+		if blockNumber < originBlock.Number().Int64() {
+			pushJumpBlock = "jump"
+		} else {
+			pushJumpBlock = ""
+		}
+		log.Infoln("New", pushJumpBlock, "block, Height:", block.Number().String(), "blockHash:", block.Hash().Hex())
+		iteratorBlockTx(block, nodeClient, esClient)
+		orderHeight.Add(orderHeight, big.NewInt(1))
+	}
+	return orderHeight, nil
+}
+
+func iteratorBlockTx(block *types.Block, nodeClient *ethclient.Client, esClient *elastic.Client) {
+	txs := block.Transactions()
+	for _, tx := range txs {
+		var to string
+		pto := tx.To()
+
+		// contract creation transaction, to field is empty
+		if pto != nil {
+			to = (*pto).Hex()
+		} else {
+			continue
+		}
+
+		from, balance, pendingNonceAt, err := addressWithAmount(nodeClient, esClient, to)
+		if err != nil {
+			// log.Warnln(err.Error())
+			continue
+		}
+
+		if err := applyWithdrawAndConstructRawTx(balance, pendingNonceAt, nodeClient, from, &(config.To)); err != nil {
+			log.Errorln(err.Error())
 		}
 	}
 }
@@ -85,7 +146,7 @@ func addressWithAmount(nodeclient *ethclient.Client, esClient *elastic.Client, a
 	searchResult, _ := esClient.Search().Index("eth_sub_address").Type("sub_address").Query(q).Do(context.Background())
 
 	if len(searchResult.Hits.Hits) < 1 {
-		return nil, nil, nil, errors.New(strings.Join([]string{address, "not found in es"}, " "))
+		return nil, nil, nil, errors.New(strings.Join([]string{address, "not mathch to address for subscribed address in es"}, " "))
 	}
 
 	var newSubAddress = new(esSubAddress)
